52a8e76d5daac210eb10d7a401f6b3d2bfd0568a
taictdev <taictdev@gmail.com>
2024-10-03 19:20:38 +0700

    Create PlayerMove

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
